65a64e4fef2e2f92f71ffe445bc7cab7f0aa0a64
Aakarshit-Dhuria <aakarshitdhuria@gmail.com>
2023-05-11 12:32:40 +0530

    Added styled components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,11 +1,48 @@
 import React, { useState } from 'react';
-
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+	margin: 0.5rem 0;
+
+	& label {
+		font-weight: bold;
+		display: block;
+		margin-bottom: 0.5rem;
+		color: ${(props) => (props.invalid ? 'red' : '')};
+	}
+
+	& input {
+		display: block;
+		width: 100%;
+		border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+		background: ${(props) => (props.invalid ? 'red' : 'transparent')};
+		font: inherit;
+		line-height: 1.5rem;
+		padding: 0 0.25rem;
+	}
+
+	& input:focus {
+		outline: none;
+		background: #fad0ec;
+		border-color: #8b005d;
+	}
+
+	&.invalid input {
+		border-color: red;
+		background-color: rgb(250, 201, 201);
+	}
+
+	&.invalid label {
+		color: red;
+	}
+`;
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState('');
 	const [isValid, setIsValid] = useState(true);
+
 	const goalInputChangeHandler = (event) => {
 		setEnteredValue(event.target.value);
 		setIsValid(true);
@@ -24,12 +61,19 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+			{/* Using styled components */}
+			<FormControl invalid={!isValid}>
 				<label>Course Goal</label>
 				<input type="text" onChange={goalInputChangeHandler} />
+			</FormControl>
 
-				{/* Setting inline styles dynamically */}
-				{/* <label style={{ color: !isValid ? 'red' : 'black' }}>
+			{/* Using dynamic css classes */}
+			{/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+				<label>Course Goal</label>
+				<input type="text" onChange={goalInputChangeHandler} /> */}
+
+			{/* Setting inline styles dynamically */}
+			{/* <label style={{ color: !isValid ? 'red' : 'black' }}>
 					Course Goal
 				</label>
 				<input
@@ -37,7 +81,7 @@ const CourseInput = (props) => {
 					type="text"
 					onChange={goalInputChangeHandler}
 				/> */}
-			</div>
+			{/* </div> */}
 			<Button type="submit">Add Goal</Button>
 		</form>
 	);
